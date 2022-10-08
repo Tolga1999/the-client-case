8dba298c9bdaa74210adb7e2797a9cfe8ea5218d
Tolga1999 <tolgadoganer@outlook.com>
2022-10-08 17:54:26 +0200

    overlay menu gemaakt

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,17 @@
 function menuOpen(){
     let overlay = document.getElementById('overlay')
+    let menuButton = document.getElementById('menuButton')
     if(overlay.style.display == 'inline'){
         overlay.style.display = 'none'
+        menuButton.style.display = 'inline'
     }else{
         overlay.style.display = 'inline'
+        menuButton.style.display = 'none'
     }
-    
+}
+
+function gaTerug(){
+    let gaTerugKnop = document.getElementById('gaTerugKnop')
+    overlay.style.display = 'none'
+    menuButton.style.display = 'inline'
 }
