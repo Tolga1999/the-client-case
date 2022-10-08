e46a5a1c0d3f0567046588f0a68dc24ba0e6f95b
Tolga1999 <tolgadoganer@outlook.com>
2022-10-08 19:13:54 +0200

    overlay menu af
    knoppen + transition toegevoegd

--- scripts/index.js
+++ scripts/index.js
@@ -1,17 +1,15 @@
+let overlay = document.querySelector('.overlay')
+let menuButton = document.getElementById('menuButton')
+let body = document.querySelector('body')
+
 function menuOpen(){
-    let overlay = document.getElementById('overlay')
-    let menuButton = document.getElementById('menuButton')
-    if(overlay.style.display == 'inline'){
-        overlay.style.display = 'none'
-        menuButton.style.display = 'inline'
-    }else{
-        overlay.style.display = 'inline'
-        menuButton.style.display = 'none'
-    }
+    overlay.classList.add('overlayOn')
+    menuButton.style.display = 'none'
+    body.style.position = 'fixed'
 }
 
 function gaTerug(){
-    let gaTerugKnop = document.getElementById('gaTerugKnop')
-    overlay.style.display = 'none'
+    overlay.classList.remove('overlayOn')
     menuButton.style.display = 'inline'
+    body.style = ''
 }
